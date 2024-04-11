73bc9f64522664aa3a0e6c58f3aa94460617d69e
MaryNkoana01 <nkoanamalebe@gmail.com>
2024-04-11 16:20:11 +0200

    Update lighthouserc.js

--- lighthouserc.js
+++ lighthouserc.js
@@ -4,6 +4,7 @@ module.exports = {
         startServerCommand: 'npm run start:dev',
         url: ['http:/localhost:4400']
       },
+      
       upload: {
         target: 'temporary-public-storage',
       },
