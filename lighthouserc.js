75980508a3407ccd13d2f03d35b32e025e6c4aaf
MaryNkoana01 <nkoanamalebe@gmail.com>
2024-04-28 14:30:58 +0200

    Update lighthouserc.js

--- lighthouserc.js
+++ lighthouserc.js
@@ -8,7 +8,7 @@ module.exports = {
       target: 'temporary-public-storage',
     },
     assert: {
-      preset: 'lighthouse:no-pwa',
+      // preset: 'lighthouse:no-pwa',
       assertions: {
         'categories:performance': ['error', { minScore: 0.9 }],
         'categories:accessibility': ['warn', { minScore: 0.9 }],
