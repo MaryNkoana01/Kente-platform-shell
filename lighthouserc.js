552bba1d0129a7b536854a7ca93046e9e94210cb
MaryNkoana01 <nkoanamalebe@gmail.com>
2024-04-28 14:10:30 +0200

    ladded the assertions

--- lighthouserc.js
+++ lighthouserc.js
@@ -7,5 +7,10 @@ module.exports = {
     upload: {
       target: 'temporary-public-storage',
     },
+    assertions: {
+      'categories:performance': ['error', { minScore: 0.9 }],
+      'categories:accessibility': ['warn', { minScore: 0.9 }],
+    },
+
   },
 };
